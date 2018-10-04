8d7a426c8259f9107384e90bc9d62dfe2aec6a81
Snakultime <snakultime@hotmail.fr>
2018-10-04 20:04:21 +0200

    Ajout du readme et des 2 exercices

---
+++ journalist.rb
@@ -0,0 +1,75 @@
+def journalistes
+    @tableaujournalistes = ["@jcunniet", "@PaulLampon", "@Aziliz31", "@ssoumier", "@marionsouzeau", "@gaellombart", "@bendarag", "@AurelieLebelle", "@julienduffe", "@thomaspoupeau", "@LilyRossignol", "@ClairGuedon", "@stephanieauguy", "@claw_prolongeau", "@_JulieMenard", "@LColcomb", "@Zlauwereys", "@MeLonguet", "@DorotheeLN", "@NolwennCosson", "@ADaboval", "@Remibaldy", "@bderveaux", "@amandechap", "@ELODIESOULIE", "@nbongarcon", "@HeloAb", "@Panamorama", "@gregplou", "@BenoitBerthe", "@LauraBruneau89", "@Anthony_Lieures", "@Sharonwaj", "@mcsonkin", "@pverduzier", "@emiliel3", "@Julien_MARION", "@SophiFay", "@bdelombre", "@annalecerf", "@AdriaBudry", "@DejNikolic", "@iJaffre", "@CyrusleVirus", "@GPardigon", "@e_vallerey", "@IsabelleSouquet", "@AudeDavidRossi", "@Yoann_Pa", "@CeliaPenavaire", "@perraultvincent", "@cboulate", "@JustineWeyl", "@Paulinejacot", "@juliechab", "@aslechevallier", "@phnou", "@Seb_Pommier", "@Alex_Bensaid", "@GuillaumeGaven", "@annelaurechouin", "@Oliviader", "@guerricp", "@JMMarchaut", "@cyceron", "@gregory_raymond", "@vhunsinger", "@l_peillon", "@fannyguinochet", "@EAssayag", "@KibweAdom", "@YvonHerry", "@JohanGarciajg", "@saidlabidi", "@lauranneprov", "@LeaDavy", "@francois_remy", "@CGuicheteau", "@FloMaillet", "@m_perroud", "@oBrunet_TSMF", "@MoonVdc", "@jc2taille", "@NellyMoussu", "@VirginK", "@b_misa", "@FabriceCouste", "@barbara_prose", "@lelia2m", "@brunoaskenazi", "@laurenechamp", "@ysisbox", "@juliengagliardi", "@PierreLel", "@kdeniau", "@_TerraInc", "@DominicArpin", "@antoinfonteneau", "@nanotousch", "@jb_roch", "@YaniKhezzar", "@Anne_Bechet", "@NCapart", "@SamyBenNaceur", "@Joumany", "@Julietteraynal", "@TGiraudet", "@SaraTanit", "@HappeFrederic", "@antoinellorca", "@michelpicot", "@Sev_Ryne", "@bobdobolino", "@murdever", "@YGrandmontagne", "@Mnyo", "@EdKOSCIANSKI", "@tnoisette", "@jankari", "@delbello_rom", "@rflechaux", "@NadiaSorelli", "@IT_Digital", "@abarbaux", "@PhilippeLeroy", "@schaptal", "@marionspee", "@lisavignoli", "@ChloeAeberhardt", "@MartineJacot", "@JuliaPascualita", "@curieusedetout", "@sgraveleau", "@bif_o", "@ElisaPineau", "@zinebdryef", "@apiquard", "@pierrehaski", "@StephanieDelmas", "@Blandine_Garot", "@vergara_i", "@evan_lebastard", "@SophieVclt", "@OlivierLevrault", "@alicedorgeval", "@LouiseMalnoy", "@alix_fx", "@pierre_baudis", "@LucMagoutier", "@AgatheMuller", "@SGianninelli", "@PaulineBoyer33", "@NaomiHalll", "@romaindlx", "@marionbr", "@Burtschy", "@JacobEtienne", "@as_lizzani", "@marie_simon", "@LaureDaussy", "@FabriceAmedeo", "@LoubnaChlaikhy", "@PlummerWilliam", "@OlivierMarin1", "@alaurefremont", "@mwesfreid", "@ChBaDe", "@pmathon", "@theobaldmarie", "@Lnpagesy", "@marclandre", "@paoliniesther", "@Feertchak", "@JBLitzler", "@GuillaumeErrard", "@quentinperinel", "@TristanQM", "@mlbo", "@constancejamet", "@LoraTerrazas", "@emiliegeffray", "@Mathilde_Sd", "@CaroPiquet", "@DCanivez", "@TIM_7375", "@blandinelc", "@ivanrioufol", "@arthurberdah", "@SarahLecoeuvre", "@guillaume_gui", "@DamienMercereau", "@W_Chloe", "@Assma_MD", "@EugenieBastie", "@HiTech_lexpress", "@bcondominas", "@Laurie_Z_", "@jeanfrancgerard", "@MathieuPagura", "@BGUYF", "@AlainPiffaretti", "@AudreyKucinskas", "@julienhory", "@Pierrefalga", "@TiphThuillier", "@cdaniez", "@LigerBaptiste", "@D_Peras", "@julie_dlb", "@Fatiha_Temmouri", "@julian2lamancha", "@GaetaneDeljurie", "@JulianMattei", "@M_Vicuna", "@DeBruynOlivier", "@Nehed_Jendoubi", "@antoine_grenapi", "@ColonnaGen", "@VictoriaGairin", "@Clement_Lacombe", "@TVigoureux", "@MargauxObriot", "@solinedelos", "@RocheSabine", "@dangerkens", "@EdouardDutour", "@MDondeyne", "@DupuisNathalie1", "@bouscarel", "@Mathieu2jean", "@Sophie_T_J", "@laurentcalixte", "@patrockwilliams", "@PascaleKremer", "@AlexJaquin", "@LauraIsaaz", "@cath_robin", "@Del_Gautherin", "@Isaduriez", "@lucietuile", "@AugeyBastien", "@mcastagnet", "@AminaKalache", "@mvaudano", "@CParrot", "@ombelinetips", "@_JoinLion", "@BarbolosiRose", "@ToiBruno1", "@FloraClodic", "@xjbdx", "@AlexiaEy", "@Emjy_STARK", "@elcoco01", "@rabilebon", "@pflovens_", "@FabriceFrossard", "@MorganeSERRES", "@MarjolaineKoch", "@edgarsnow", "@SRNLF", "@CChassigneux", "@KerinecMoran", "@NassiraELM", "@NewsRicard", "@Sandreene", "@Emilezrt", "@Pierre_Do", "@Micode", "@CColumelli", "@DavidAbiker", "@ClementBergantz", "@benjaminrabier", "@celinekallmann", "@edwyplenel", "@C_Barbier", "@JJBourdin_RMC", "@LaurenceFerrari", "@aslapix", "@IsaMillet", "@MaximeSwitek", "@tomjoubert", "@jszanchi", "@roqueeva", "@XavierBiseul", "@florencesantrot", "@AntoineCrochet", "@freeman59", "@MaudeML", "@philippe_gulpi", "@mathieum76", "@kiouari", "@imanemoustakir", "@BenedicteMallet", "@Emilie_Brouze", "@antoinebarret", "@_nicolasbocquet", "@remibuhagiar", "@CourretB", "@AymericRobert", "@miraelmartins", "@pmaniere", "@jesuisraphk", "@David_Ingram", "@pcelerier", "@technomedia", "@Geraldinedauver", "@ThierryLabro", "@Newsdusud", "@nrauline", "@gbregeras", "@SCouasnonBFM", "@actualites_nrv", "@dimitrimoulins", "@oli_aura", "@FabieChiche", "@Vincent_Raimblt", "@ChristophGreuet", "@PAlbuchay", "@MarrauddesGrot", "@vtalmon", "@cedric", "@olivierfrigara", "@Julien_Jay", "@LydiaBerroyer", "@Shuua", "@datisdaz", "@Steuph", "@ameliecharnay", "@Bruno_LesNums", "@LelloucheNico", "@CciliaDiQuinzio", "@Elodie_C", "@SylvRolland", "@Kocobe", "@FL_Debes", "@jdupontcalbo", "@GarciaVictor_", "@NicoRichaud", "@RHoueix", "@simottel", "@DamienLicata", "@annabelle_L", "@Lea_Lejeune", "@axel_deb", "@marin_eben", "@ptiberry", "@MatthieuDelach", "@sandrinecassini", "@benjaminferran", "@ppgarcia75", "@NotPatrick", "@ivalerio", "@FabienneSchmitt", "@alexgoude", "@JeromeColombain", "@manhack", "@Capucine_Cousin", "@Fsorel", "@oliviertesquet", "@marjoriepaillon", "@ginades", "@PierreTran", "@DelphineCuny", "@reesmarc", "@lauratenoudji", "@ldupin", "@carolinedescham", "@Lucile_Quillet", "@cgabizon", "@Allocab", "@epenser", "@JAGventeprivee", "@frwrds", "@Laure__Bourdon", "@Xavier75", "@maximeverner", "@s_jourdain", "@LoriHelloc"]
+end
+
+def nbr_de_journalist
+ puts "Il y a #{@tableaujournalistes.size.to_i} journalistes dans la liste."
+end
+
+
+def number_in_array
+array_journalist_number = []
+@tableaujournalistes.each do |handle|
+       if handle.include?('0') || handle.include?('1') || handle.include?('2') || handle.include?('3') || handle.include?('4') || handle.include?('5') || handle.include?('6') || handle.include?('7') || handle.include?('8') || handle.include?('9')
+array_journalist_number.push handle
+       end
+       end
+       puts "il y a #{array_journalist_number.size.to_i} qui ont un chiffre dans leur pseudo twitter"
+end
+
+def search_aude
+ aude = []
+ @tableaujournalistes.map(&:downcase).each do |handle|
+       if handle.include?('aude')
+         aude.push handle
+       end
+       end
+       puts "Il y a #{aude.size.to_i} qui ont Aude dans leur nom."
+end
+
+def search_maj
+  cap = []
+  @tableaujournalistes.each do |handle|
+    if handle[1] =~ /[A-Z]/
+      cap.push handle
+    end
+  end
+  puts "Il y a #{cap.size.to_i} pseudos qui commence avec une majuscule."
+end
+
+def contain_maj
+  maj = []
+  @tableaujournalistes.each do |handle|
+      if handle =~ /[A-Z]/
+        maj.push handle
+      end
+    end
+    puts "Il y a #{maj.size.to_i} pseudos qui contiennent des majuscules."
+end
+
+def search__
+  space = []
+  @tableaujournalistes.each do |handle|
+      if handle =~ /[_]/
+        space.push handle
+      end
+    end
+    puts "Il y a #{space.size.to_i} noms qui contiennent des _."
+end
+
+def alph
+    puts @tableaujournalistes.sort
+end
+
+def perform
+ journalistes
+ nbr_de_journalist
+ number_in_array
+ search_aude
+ search_maj
+ contain_maj
+ search__
+ alph
+end
+
+perform
---
+++ readme.md
@@ -0,0 +1,6 @@
+Voici les exercices de la Semaine 1 Jour 4 !
+
+Fait par Axel // @Axel
+
+Avec l'aide de Cédric // @iCedric
+et Jonathan // @JonathanMete
